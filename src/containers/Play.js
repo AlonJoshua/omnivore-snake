66cb0a0a3ae2f5a9c45417b01f0c697597f3b669
Alon Joshua <alonjoshua@gmail.com>
2020-04-22 17:01:31 +0300

    changed movement system to color, WIP on tail movement

--- src/containers/Play.js
+++ src/containers/Play.js
@@ -1,33 +1,68 @@
 import React, { Component, Fragment } from 'react';
 import '../App.css';
 import Navbar from '../components/Navbar';
-import Snake from './Snake';
 
 class Play extends Component {
 	constructor(props) {
 		super(props)
+		this.state ={
+			gameSqueres: []
+		}
+	}
+
+		snakeMoves = (e) => {
+		const { gameSqueres } = this.state;
+		const arrowKey = e.keyCode;
+		const findHead = (div) => div.className === 'head';
+		const headIndex = gameSqueres.findIndex(findHead);
+		switch (arrowKey) {
+			case 38:
+				gameSqueres[headIndex - 10].className = 'head';
+				gameSqueres[headIndex].className = 'play-div';
+				break;
+			case 39:
+				gameSqueres[headIndex + 1].className = 'head';
+				gameSqueres[headIndex].className = 'play-div';
+				break;
+			case 37:
+				gameSqueres[headIndex - 1].className = 'head';
+				gameSqueres[headIndex].className = 'play-div';
+				break;
+			case 40:
+				gameSqueres[headIndex + 10].className = 'head';
+				gameSqueres[headIndex].className = 'play-div';
+				break;
+			default:
+				break;
+		}
 	}
 
 	componentDidMount() {
 		// fill play area grid with divs
 		const playArea = document.getElementById('container');
-		const snakeHead = document.getElementById('snake-head');
-		const snakeBody = document.getElementById('snake-body');
-		const divArray = [snakeHead, snakeBody];
+		// const snakeHead = document.getElementById('snake-head');
+		// const snakeBody = document.getElementById('snake-body');
+		const divArray = [];
 		let order = 0;
+		let id = 1;
 		while (divArray.length < 130) {
-			if (order === parseInt(divArray[0].style.order) || 
-				order === parseInt(divArray[1].style.order)) {
-			order +=1;
-			} else {
 				const newDiv = document.createElement('div');
 				newDiv.style.order = order;
+				newDiv.id = id.toString();
 				newDiv.className = 'play-div';
 				playArea.appendChild(newDiv);
-				divArray.push('newDiv');
-				order += 1;	
-			}
+				divArray.push(newDiv);
+				order += 1;
+				id += 1;
 		}
+		this.setState({gameSqueres: divArray});
+		setTimeout(() => {
+			document.addEventListener("keydown", this.snakeMoves, false);
+			const head = document.getElementById('106');
+			head.className = 'head';
+			document.getElementById('116').className = 'body';
+			document.getElementById('126').className = 'body';
+		}, 200);
 	}
 
 	render() {
@@ -39,12 +74,9 @@ class Play extends Component {
 				id='container'
 				>
 				{/*
+				1) create a body for the snake that moves after him
 				
-				1) tail moves after the snake
-
 				 */}
-				<Snake
-				/>
 				</div>
 			</Fragment>
 		)
