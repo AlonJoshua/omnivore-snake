6c93c1e856bd6241b64eb7b54dc8dc6f471e8261
Alon Joshua <alonjoshua@gmail.com>
2020-05-04 15:47:26 +0300

    Define borders

--- src/containers/Play.js
+++ src/containers/Play.js
@@ -11,14 +11,19 @@ class Play extends Component {
 	}
 		// snake move system: based on grid div color
 		snakeMoves = (e) => {
-		const { gameSqueres, snake } = this.state;
+		const { gameSqueres, snake, lastMove } = this.state;
 		const arrowKey = e.keyCode;
 
 		const findHead = (div) => div.className === 'head';
 		const headIndex = gameSqueres.findIndex(findHead);
+		const headId = gameSqueres[headIndex].id;
 
 		const findTail = (div) => div.id === snake[snake.length-1].id;
 		const tailIndex = gameSqueres.findIndex(findTail);
+
+		const findRightBorder = (div) => div.className === 'play-div' && div.id % 10 === 0;
+
+		const findLeftBorder = (div) => div.className === 'play-div' && div.id % 10 - 1 === 0;
 
 		const newBodyArr = () => {
 			snake[1].className = 'body';
@@ -26,26 +31,71 @@ class Play extends Component {
 			snake.pop()
 		}
 
-		switch (arrowKey) {
-			case 38:
-				gameSqueres[headIndex - 10].className = 'head';
+		// move functions
+		const movesUp = () => {
+			// can't cross top border
+			if (!gameSqueres[headIndex - 10]) {
+				// this.setState({snake: lastMove});
+				console.log('game over: top cross')
+			} else {
+				gameSqueres[headIndex - 10].className = 'head'
 				snake.unshift(gameSqueres[headIndex - 10]);
 				newBodyArr();
-				break;
-			case 39:
-				gameSqueres[headIndex + 1].className = 'head';
+			}
+		}
+
+		const movesRight = () => {
+			// can't skip to left border
+			const leftBorderArr = gameSqueres.filter(findLeftBorder).map(e => e.id);
+			if (leftBorderArr.some(id => parseInt(id) === parseInt(headId) + 1)) {
+				console.log('game over: right cross')
+			} else if (!gameSqueres[headIndex + 1]) {
+				console.log('game over: right cross')
+			} else {
+				gameSqueres[headIndex + 1].className = 'head'
 				snake.unshift(gameSqueres[headIndex + 1]);
 				newBodyArr();
-				break;
-			case 37:
-				gameSqueres[headIndex - 1].className = 'head';
-				snake.unshift(gameSqueres[headIndex - 1]);
-				newBodyArr();
-				break;
-			case 40:
-				gameSqueres[headIndex + 10].className = 'head';
+			}
+		}
+
+		const movesDown = () => {
+			// can't cross bottom border
+			if (!gameSqueres[headIndex + 10]) {
+				// this.setState({snake: lastMove});
+				console.log('game over: bottom cross')
+			} else {
+				gameSqueres[headIndex + 10].className = 'head'
 				snake.unshift(gameSqueres[headIndex + 10]);
 				newBodyArr();
+			}
+		}
+
+		const movesLeft = () => {
+			// can't cross left border
+			const rightBorderArr = gameSqueres.filter(findRightBorder).map(e => e.id);
+			if (rightBorderArr.some(id => parseInt(id) === parseInt(headId) - 1)) {
+				console.log('game over: left cross')
+			} else if (!gameSqueres[headIndex - 1]) {
+				console.log('game over: left cross')
+			} else {
+				gameSqueres[headIndex - 1].className = 'head'
+				snake.unshift(gameSqueres[headIndex - 1]);
+				newBodyArr();
+			}
+		}
+
+		switch (arrowKey) {
+			case 38:
+				movesUp();
+				break;
+			case 39:
+				movesRight();
+				break;
+			case 37:
+				movesLeft();
+				break;
+			case 40:
+				movesDown();
 				break;
 			default:
 				break;
@@ -55,7 +105,6 @@ class Play extends Component {
 
 	componentDidMount() {
 		// fill play area grid with divs
-		const { gameSqueres } = this.state;
 		const playArea = document.getElementById('container');
 		const divArray = [];
 		let order = 0;
@@ -70,7 +119,11 @@ class Play extends Component {
 				order += 1;
 				id += 1;
 		}
-		this.setState({gameSqueres: divArray, snake: [divArray[105], divArray[115], divArray[125]]});
+		this.setState({
+			gameSqueres: divArray, 
+			snake: [divArray[105], divArray[115], divArray[125]],
+			lastMove: [divArray[105], divArray[115], divArray[125]]
+		});
 
 		// create snake head and body
 
@@ -91,7 +144,7 @@ class Play extends Component {
 				id='container'
 				>
 				{/*
-				1) every time the head moves, the body squares will move accordingly
+				1) make moves valid with no border or body cross
 				
 				 */}
 				</div>
